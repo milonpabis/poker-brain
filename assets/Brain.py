33a5b87f81533a35e794efb1746ab55f87b00d3d
milonpabis <miloszpabis@onet.pl>
2023-11-29 11:56:20 +0100

    scheme of working

--- assets/Brain.py
+++ assets/Brain.py
@@ -2,6 +2,7 @@ import Card
 import Deck
 import Hand
 import Board
+from multipledispatch import dispatch
 
 class Brain:
 
@@ -10,12 +11,74 @@ class Brain:
         self.board = Board()
         self.hand = Hand()
 
+
+    @dispatch(Card)
+    def add_hand_card(self, card):  # adds a card to the hand
+        if len(self.hand.get_cards()) < 2:
+            self.hand.add_card(self.deck.remove_card(card))
+    
+
+    @dispatch(str, int)
+    def add_hand_card(self, suit, value):
+        if len(self.hand.get_cards()) < 2:
+            self.hand.add_card(self.deck.remove_card(suit, value))
+
+
+    @dispatch(Card)
+    def remove_hand_card(self, card):   # removes a card from the hand
+        if len(self.hand.get_cards()):
+            self.hand.remove_card(card)
+            self.deck.add_card(card)
+
+
+    @dispatch(str, int)
+    def remove_hand_card(self, suit, value):
+        if len(self.hand.get_cards()):
+            self.hand.remove_card(suit, value)
+            self.deck.add_card(suit, value)
+
+
+    @dispatch(Card)
+    def add_board_card(self, card): # adds a card to the board
+        if len(self.board.get_cards()) < 5:
+            self.board.add_card(self.deck.remove_card(card))
+
+
+    @dispatch(str, int)
+    def add_board_card(self, suit, value):
+        if len(self.board.get_cards()) < 5:
+            self.board.add_card(self.deck.remove_card(suit, value))
+
+
+
+
+
+
+
+
+
+
     def how_many_cards(self, value=None, suit=None):    # returns the number of specific cards in the deck
         if not value:
-            ...
+                 # returns the number of cards with the specific suit  // int
+            return len(self.deck.return_suit(suit))
         elif not suit:
-            ...
+                 # returns the number of cards with the specific value  // int
+            return len(self.deck.return_ranks(value))
         else:
-            ...
+                 # returns if card with the specific value and suit is in the deck    // bool 0 1
+            return len(self.deck.return_ranks(value)) and len(self.deck.return_suit(suit))
+
+
+    def calc_odds(self, value=None, suit=None):     # returns the odds of drawing a specific card
+        if not value:
+            return self.how_many_cards(suit=suit) / len(self.deck.get_cards())
+        elif not suit:
+            return self.how_many_cards(value=value) / len(self.deck.get_cards())
+        else:
+            return self.how_many_cards(value=value, suit=suit) / len(self.deck.get_cards())
+    
+
+    
 
     
